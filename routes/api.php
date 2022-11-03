4e8e9fe1ee746ee18f3804545d5ff72e41549a1f
JuanFe622 <juanf.herrerac@autonoma.edu.co>
2022-11-03 00:44:21 -0500

    Route aggregation for the frontend

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('v1/owner', [OwnerController::class, 'store']);
     Route::post('v1/vehicle', [VehicleController::class, 'store']);
 
+    Route::get('v1/owners', [OwnerController::class, 'index']);
     Route::get('v1/vehicles', [VehicleController::class, 'index']);
+    Route::get('v1/slots?available', [SlotController::class, 'index']);
 
     Route::post('v1/bill', [BillController::class, 'store']);
     Route::get('v1/bill/{bill_id}', [BillController::class, 'update']);
